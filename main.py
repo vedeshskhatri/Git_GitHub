a43f2835cc23a2f85f8e6b298e1e753e5f32dcbf
Vedesh S Khatri <khatrivedesh@gmail.com>
2025-06-05 19:44:01 +0530

    Update message in main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello from Git!")
+import random
+
+def generate_random_number(start=1, end=100):
+    return random.randint(start, end)
+
+if __name__ == "__main__":
+    number = generate_random_number()
+    print(f"Random Number between 1 and 100: {number}")
